783daf2d2df579a387bd89e3974ca9387bb389e6
djankooo <218202@student.pwr.edu.pl>
2019-06-11 21:24:41 +0200

    v2?

--- src/main/java/jmx/Remote.java
+++ src/main/java/jmx/Remote.java
@@ -1,5 +1,8 @@
 package jmx;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 public class Remote implements RemoteMBean {
 
     App app;
@@ -12,7 +15,7 @@ public class Remote implements RemoteMBean {
     @Override
     public void togleLights() {
         System.out.println("Remote togle lights     : " + this.app);
-        app.togleLightsIN();
+        this.app.togleLightsIN();
     }
 
     @Override
@@ -22,12 +25,26 @@ public class Remote implements RemoteMBean {
 
     @Override
     public void openWindow() {
-        app.launch(App.class);
         System.out.println("Remote get lamps        : " + this.app);
+//        TO SSIE PALKE
+//        try {
+//            Stage addingStage = new Stage();
+//            app.start(addingStage);
+//            addingStage.show();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+//        TO TEZ SSIE PALE
+//          this.app.main(new String[]{});
+
+//        I TO TEZ SSIE PALKE
+        this.app.launch(App.class);
+
+
     }
 
     Remote(){
         this.app = new App();
-
     }
 }
